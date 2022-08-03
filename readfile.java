779056f602c4b8b86492a5aeb1bfc9a32c63d1c8
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-03 16:53:32 +0530

    Add files via upload

---
+++ readfile.java
@@ -0,0 +1,35 @@
+﻿package com.company;
+import java.awt.*;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.io.File;
+class readfile
+{
+    public static void read() throws IOException
+    {
+        try
+        {
+            File myname = new File("C:\\Users\\ELCOT\\Saibaba.txt");
+            Scanner myreader = new Scanner(myname);
+            while(myreader.hasNextLine())
+            {
+                String line = myreader.nextLine();
+                System.out.println(line);
+            }
+            myreader.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("File not found");
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        readfile read = new readfile();
+        read.read();
+    }
+}
